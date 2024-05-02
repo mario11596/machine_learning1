417578e7708ab3f07a4a6ffddbd0735deac1be97
Muresan Gabriel Alexander <muresangabriel.alexandru@gmail.com>
2024-05-02 04:01:15 +0200

    all tasks done

--- lin_reg_memristors.py
+++ lin_reg_memristors.py
@@ -11,7 +11,7 @@ class MemristorFault(Enum):
 
 
 def model_to_use_for_fault_classification():
-    return None # TODO: change this to either 1 or 2 (depending on which model you decide to use)
+    return 2
 
 
 def fit_zero_intercept_lin_model(x: np.ndarray, y: np.ndarray) -> float:
@@ -22,7 +22,11 @@ def fit_zero_intercept_lin_model(x: np.ndarray, y: np.ndarray) -> float:
     """
 
     # TODO: implement the equation for theta containing sums
-    theta = None
+    delta_R_ideal = x
+    delta_R = y
+    numerator = np.sum(delta_R * delta_R_ideal)
+    denominator = np.sum(delta_R_ideal ** 2)
+    theta = numerator / denominator
     return theta
 
 
@@ -47,8 +51,27 @@ def fit_lin_model_with_intercept(x: np.ndarray, y: np.ndarray) -> Tuple[float, f
     """
 
     # TODO: implement the equation for theta_0 and theta_1 containing sums
-    theta_0 = None
-    theta_1 = None
+    sum_x = np.sum(x)
+    sum_y = np.sum(y)
+    sum_xx = np.sum(x * x)
+    sum_xy = np.sum(x * y)
+    n = len(x)
+
+    det_A = n * sum_xx - sum_x * sum_x
+
+    if det_A != 0:
+        # Inverse of matrix
+        mat_inv = (1 / det_A) * np.array([[sum_xx, -sum_x],
+                                        [-sum_x, n]])
+
+        # Multiplying the inverse of A with b
+        theta = mat_inv @ np.array([sum_y, sum_xy])
+        theta_0, theta_1 = theta[0], theta[1]
+    else:
+        # Handle the non-invertible case
+        print("Matrix A is singular and cannot be inverted.")
+        theta_0, theta_1 = None, None
+
     return theta_0, theta_1
 
 
@@ -74,13 +97,15 @@ def classify_memristor_fault_with_model2(theta0: float, theta1: float) -> Memris
     :param theta1: the slope parameter of the linear model
     :return: the type of fault
     """
-    # TODO: Implement either this function, or the function `classify_memristor_fault_with_model1`,
-    #       depending on which model you decide to use.
+    margin = 0.5
+    if theta1 > 1 + margin:
+        fault_type = MemristorFault.CONCORDANT
+    elif theta1 < -1 - margin:
+        fault_type = MemristorFault.DISCORDANT
+    elif -1 + margin < theta1 < 1 - margin:
+        fault_type = MemristorFault.STUCK
+    else:
+        fault_type = MemristorFault.IDEAL
 
-    # If you decide to use this function, remove the line `raise NotImplementedError()` and
-    # return a MemristorFault based on the value of theta0 and theta1.
-    # For example, return MemristorFault.IDEAL if you decide that the given theta pair
-    # does not indicate a fault, and so on.
-    # Use if-statements and choose thresholds for the parameters that make sense to you.
+    return fault_type
 
-    raise NotImplementedError()
--- main.py
+++ main.py
@@ -22,14 +22,14 @@ def task_1():
     # Load the data
     data = np.load('data/memristor_measurements.npy')
     print(data.shape)
-    
+
     n_memristor = data.shape[0]
 
     ### --- Use Model 1 (zero-intercept lin. model, that is, fit the model using fit_zero_intercept_lin_model)    
     estimated_params_per_memristor_model1 = np.zeros(n_memristor)
     for i in range(n_memristor):
-        x, y = None, None # TODO: extract the corresponding entries from the data array
-        theta = None # TODO: call the appropriate function with x and y
+        x, y = data[i][:, 0], data[i][:, 1]
+        theta = fit_zero_intercept_lin_model(x, y)
         estimated_params_per_memristor_model1[i] = theta
 
     # Visualize the data and the best fit for each memristor
@@ -41,8 +41,8 @@ def task_1():
     ### --- Use Model 2 (lin. model with intercept, that is, fit the model using fit_lin_model_with_intercept)    
     estimated_params_per_memristor_model2 = np.zeros((n_memristor, 2))
     for i in range(n_memristor):
-        x, y = None, None # TODO: extract the corresponding entries from the data array
-        theta0, theta1 = None, None # TODO: call the appropriate function with x and y
+        x, y = data[i][:, 0], data[i][:, 1]
+        theta0, theta1 = fit_lin_model_with_intercept(x, y)
         # TODO: If you have implemented the bonus task, you can call the bonus function instead to check
         #  if the results are the same
         estimated_params_per_memristor_model2[i, :] = [theta0, theta1]
@@ -54,8 +54,6 @@ def task_1():
     print(f"Estimated params (theta_0, theta_1) per memristor: {estimated_params_per_memristor_model2}")
 
     fault_types = []
-    # TODO: Use either Model 1 or Model 2 for the decision on memristor fault type.
-    #  Your choice should be reflected in the `model_to_use_for_fault_classification` function.
     model_to_use = model_to_use_for_fault_classification()
     for i in range(n_memristor):
         if model_to_use == 1:
@@ -70,7 +68,7 @@ def task_1():
 
     print(f'\nClassifications (based on Model {model_to_use})')
     for i, fault_type in enumerate(fault_types):
-        print(f'Memristor {i+1} is classified as {fault_type}.')
+        print(f'Memristor {i + 1} is classified as {fault_type}.')
 
 
 def task_2():
@@ -80,18 +78,18 @@ def task_2():
         print(f'---- Logistic regression task {task} ----')
         if task == 1:
             # TODO: Load the data set 1 (X-1-data.npy and targets-dataset-1.npy)
-            X_data = np.load("data/X-1-data.npy") # TODO: change me
-            y = np.load("data/targets-dataset-1.npy") # TODO: change me
+            X_data = np.load("data/X-1-data.npy")  # TODO: change me
+            y = np.load("data/targets-dataset-1.npy")  # TODO: change me
             create_design_matrix = create_design_matrix_dataset_1
         elif task == 2:
             # TODO: Load the data set 2 (X-1-data.npy and targets-dataset-2.npy)
-            X_data = np.load("data/X-1-data.npy") # TODO: change me
-            y = np.load("data/targets-dataset-2.npy") # TODO: change me
+            X_data = np.load("data/X-1-data.npy")  # TODO: change me
+            y = np.load("data/targets-dataset-2.npy")  # TODO: change me
             create_design_matrix = create_design_matrix_dataset_2
         elif task == 3:
             # Load the data set 3 (X-2-data.npy and targets-dataset-3.npy)
-            X_data = np.load("data/X-2-data.npy") # TODO: change me
-            y = np.load("data/targets-dataset-3.npy") # TODO: change me
+            X_data = np.load("data/X-2-data.npy")  # TODO: change me
+            y = np.load("data/targets-dataset-3.npy")  # TODO: change me
             create_design_matrix = create_design_matrix_dataset_3
         else:
             raise ValueError('Task not found.')
@@ -104,7 +102,8 @@ def task_2():
         # TODO: Split the dataset using the `train_test_split` function.
         #  The parameter `random_state` should be set to 0.
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-        print(f'Shapes of: X_train {X_train.shape}, X_test {X_test.shape}, y_train {y_train.shape}, y_test {y_test.shape}')
+        print(
+            f'Shapes of: X_train {X_train.shape}, X_test {X_test.shape}, y_train {y_train.shape}, y_test {y_test.shape}')
 
         # Train the classifier
         custom_params = logistic_regression_params_sklearn()
@@ -112,14 +111,15 @@ def task_2():
         clf = LogisticRegression(**custom_params)
         # TODO: Fit the model to the data using the `fit` method of the classifier `clf`
         clf.fit(X_train, y_train)
-        acc_train, acc_test = clf.score(X_train, y_train), clf.score(X_test, y_test) # TODO: Use the `score` method of the classifier `clf` to calculate accuracy
+        acc_train, acc_test = clf.score(X_train, y_train), clf.score(X_test,
+                                                                     y_test)  # TODO: Use the `score` method of the classifier `clf` to calculate accuracy
 
         print(f'Train accuracy: {acc_train * 100:.2f}%. Test accuracy: {100 * acc_test:.2f}%.')
 
-        yhat_train = clf.predict_proba(X_train) # TODO: Use the `predict_proba` method of the classifier `clf` to
-                          #  calculate the predicted probabilities on the training set
-        yhat_test = clf.predict_proba(X_test) # TODO: Use the `predict_proba` method of the classifier `clf` to
-                         #  calculate the predicted probabilities on the test set
+        yhat_train = clf.predict_proba(X_train)  # TODO: Use the `predict_proba` method of the classifier `clf` to
+        #  calculate the predicted probabilities on the training set
+        yhat_test = clf.predict_proba(X_test)  # TODO: Use the `predict_proba` method of the classifier `clf` to
+        #  calculate the predicted probabilities on the test set
 
         # TODO: Use the `log_loss` function to calculate the cross-entropy loss
         #  (once on the training set, once on the test set).
@@ -132,7 +132,7 @@ def task_2():
 
         plot_logistic_regression(clf, create_design_matrix, X_train, f'(Dataset {task}) Train set predictions',
                                  figname=f'logreg_train{task}')
-        plot_logistic_regression(clf, create_design_matrix, X_test,  f'(Dataset {task}) Test set predictions',
+        plot_logistic_regression(clf, create_design_matrix, X_test, f'(Dataset {task}) Test set predictions',
                                  figname=f'logreg_test{task}')
 
         # TODO: Print theta vector (and also the bias term). Hint: Check the attributes of the classifier
@@ -148,38 +148,87 @@ def task_3():
 
     # TODO: Choose a random starting point using samples from a standard normal distribution
     normal_distribution = np.random.randn(2)
+    normal_distribution = np.random.randn(2)
     x0 = normal_distribution[0]
     y0 = normal_distribution[1]
     print(f'{x0:.4f}, {y0:.4f}')
 
     # TODO: Call the function `gradient_descent` with a chosen configuration of hyperparameters,
     #  i.e., learning_rate, lr_decay, and num_iters. Try out lr_decay=1 as well as values for lr_decay that are < 1.
-    x, y, f_list = gradient_descent(ackley, gradient_ackley, x0, y0, 1.0, 0.995, 100)
+    configurations = [
+        {'learning_rate': 0.1, 'lr_decay': 0.99, 'num_iters': 500},
+        {'learning_rate': 0.05, 'lr_decay': 0.995, 'num_iters': 1000},
+        {'learning_rate': 0.01, 'lr_decay': 1.0, 'num_iters': 200},
+        {'learning_rate': 0.2, 'lr_decay': 0.90, 'num_iters': 300},
+        {'learning_rate': 0.2, 'lr_decay': 0.90, 'num_iters': 100},
+        {'learning_rate': 0.2, 'lr_decay': 0.95, 'num_iters': 50},
+        {'learning_rate': 0.01, 'lr_decay': 0.95, 'num_iters': 1000}
+    ]
+    results = []
 
-    # Print the point that is found after `max_iter` solution
-    print(f'{x:.4f}, {y:.4f}')
+    for config in configurations:
+        LEARNING_RATE = config['learning_rate']
+        LR_DECAY = config['lr_decay']
+        ITTER = config['num_iters']
+        x, y, f_list = gradient_descent(ackley, gradient_ackley, x0, y0, LEARNING_RATE, LR_DECAY, ITTER)
+
+        results.append({
+            'config': config,
+            'final_f': ackley(x, y),
+            'final_pos': (x, y),
+            'convergence_curve': f_list,
+            'f_list': f_list
+        })
+
+    def generate_result_table(results):
+        # HEADER
+        latex_table = """\\begin{table}[H]
+        \\centering
+        \\begin{tabular}{|c|c|c|c|c|c|}
+        \\hline
+        \\textbf{Learning Rate} & \\textbf{LR Decay} & \\textbf{Iterations} & 
+        \\textbf{Final Function Value} & \\textbf{Final Pos X} & \\textbf{Final Pos Y} \\\\\\hline
+        """
+        # CONTENT
+        for result in results:
+            learning_rate = result['config']['learning_rate']
+            lr_decay = result['config']['lr_decay']
+            num_iters = result['config']['num_iters']
+            final_f = result['final_f']
+            x = result['final_pos'][0]
+            y = result['final_pos'][1]
+            latex_table += (f"{learning_rate} & {lr_decay} & {num_iters} & "
+                            f"{final_f:.6f} & {x:.6f} & {y:.6f} \\\\\\hline\n")
+        # CLOSING  BLOCK
+        latex_table += """\\end{tabular}
+        \\caption{Results of Gradient Descent Test Battery}
+        \\label{tab:gradient_descent_results}
+        \\end{table}
+        """
+        return latex_table
+
+    latex_code = generate_result_table(results)
 
     # TODO: Use `f_list` to create a plot of the function over iteration.
     #  Do not forget to label the plot (xlabel, ylabel, title).
-
-    plt.plot(f_list, color = 'blue')
+    f_list = results[3]["f_list"]
+    plt.plot(f_list, color='blue')
 
     plt.xlabel('Iteration')
     plt.ylabel('Cost values')
     plt.title('The changes of the cost over iteration')
     plt.savefig('plots/gradient_descent_cost.pdf')
 
-
-    print(f'Solution found: f({x:.4f}, {y:.4f})= {ackley(x,y):.4f}' )
-    print(f'Global optimum: f(0, 0)= {ackley(0,0):.4f}')
+    print(f'Solution found: f({x:.4f}, {y:.4f})= {ackley(x, y):.4f}')
+    print(f'Global optimum: f(0, 0)= {ackley(0, 0):.4f}')
 
 
 def main():
     np.random.seed(33761)
 
-    #task_1()
-    task_2()
-    #task_3()
+    # task_1()
+    # task_2()
+    task_3()
 
 
 if __name__ == '__main__':
